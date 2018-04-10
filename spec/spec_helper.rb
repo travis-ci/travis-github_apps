0bc373631809198300da011373ff58bb6d5717dd
Kerri Miller <kerrizor@kerrizor.com>
2018-04-10 13:53:54 -0700

    Use mocha as the mock framework

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "bundler/setup"
 require "travis/github_apps"
 
 RSpec.configure do |config|
+  config.mock_with :mocha
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
