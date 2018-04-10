9514febf29ed0fccc1f9e4021623b66b9cda8012
Kerri Miller <kerrizor@kerrizor.com>
2018-04-10 13:25:11 -0700

    Boilerplate gem from '$ bundle gem travis-github_apps'

---
+++ lib/travis/github_apps.rb
@@ -0,0 +1,7 @@
+require "travis/github_apps/version"
+
+module Travis
+  module GithubApps
+    # Your code goes here...
+  end
+end
---
+++ lib/travis/github_apps/version.rb
@@ -0,0 +1,5 @@
+module Travis
+  module GithubApps
+    VERSION = "0.1.0"
+  end
+end
